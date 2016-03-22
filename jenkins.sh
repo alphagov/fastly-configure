78ac4098cf05a9f89d7a0d36c9cf014ad062cac4
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-03-22 17:28:06 +0000

    Use VCL templates from public GitHub repository

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,10 @@
 git clone 'git@github.gds:gds/cdn-configs.git'
 
 cp cdn-configs/fastly/fastly.yaml .
-cp cdn-configs/fastly/vcl_templates/* vcl_templates/
+
+git clone 'git@github.com:alphagov/govuk-cdn-config.git'
+
+cp govuk-cdn-config/vcl_templates/*.vcl.erb vcl_templates/
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 bundle exec ./deploy_vcl ${vhost} ${ENVIRONMENT}
