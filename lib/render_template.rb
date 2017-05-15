e8334165fefc14b40739041bb88e0b88111de480
Brenetic <b@brenetic.com>
2017-05-15 12:51:00 +0000

    Add A/B tests configuration
    A/B tests should now be added/changed in `ab_tests/ab_tests.yaml`. It is
    also possible to make changes to the percentage for b group and the expiration
    time for the cookies. It uses the vcl template (here)[https://github.com/alphagov/govuk-cdn-config/blob/master/vcl_templates/www.vcl.erb]
    which has had some dynamic rendering added to it in (this
    PR)[https://github.com/alphagov/govuk-cdn-config/pull/52].
    
    We don't take into account multivariate testing at this stage.
    
    Also added configuration for the upcoming `SearchMatchLength` A/B tests.

--- lib/render_template.rb
+++ lib/render_template.rb
@@ -1,6 +1,10 @@
+require 'yaml'
+
 class RenderTemplate
   def self.render_template(configuration, environment, config, version)
+    # Both config and ab_tests are used inside the vcl.erb template
     vcl_file = File.join(File.dirname(__FILE__), '..', 'vcl_templates', "#{configuration}.vcl.erb")
+    ab_tests = YAML.load_file(File.join(__dir__, '..', 'ab_tests', 'ab_tests.yaml'))
     ERB.new(File.read(vcl_file), nil, '-').result(binding)
   end
 end
