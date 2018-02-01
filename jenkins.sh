1aee4fc78a0e7219b0c06359d1b591912b46c7de
Tijmen Brommet <tijmen@gmail.com>
2018-02-01 15:22:16 +0000

    **

--- jenkins.sh
+++ jenkins.sh
@@ -8,6 +8,8 @@ cp cdn-configs/fastly/fastly.yaml .
 
 git clone 'git@github.com:alphagov/govuk-cdn-config.git'
 
+cd govuk-cdn-config && git checkout preconnect && cd -
+
 cp govuk-cdn-config/vcl_templates/*.vcl.erb vcl_templates/
 
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
